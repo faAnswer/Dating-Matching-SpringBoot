0c47bc3180c4197f47161d3fac084157749899e0
Oscar SO <oscarogc@gmail.com>
2022-11-07 16:25:16 +0800

    first commit

---
+++ src/main/java/org/tecky/nohorny/NohornyApplication.java
@@ -0,0 +1,13 @@
+package org.tecky.nohorny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NohornyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NohornyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tecky/nohorny/NohornyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tecky.nohorny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NohornyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
