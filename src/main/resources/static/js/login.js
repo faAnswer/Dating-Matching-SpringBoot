680ceb7dfc5798ed35cc5cc85a0fdd44c5e96d5e
Ariel <arielyu.tc@gmail.com>
2022-11-14 15:19:20 +0800

    hello on9

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,76 @@
+let params = new URLSearchParams(location.search)
+let nextPageHref = params.get('redirect_uri')
+
+//register function
+async function signup(event) {
+    event.preventDefault()
+    let form = event.target
+
+    try {
+        let url = form.action.replace(location.origin, API_ORIGIN)
+        let res = await fetch(url, {
+            method: form.method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nickname: registerName.value,
+                username: registerUsername.value,
+                email: registerEmail.value,
+                password: registerPassword.value,
+                RepeatPassword: registerRepeatPassword.value,
+
+
+            })
+        })
+        console.log('post signup response:', res.status, res.statusText)
+        message.textContent = res.statusText
+
+        let json = await res.json() //res.json = put value in json file
+
+        if (json.error) {
+            message.textContent = json.error
+        } else {
+            location.href = nextPageHref
+        }
+    } catch (error) {
+        console.error('failed to post signup:', error)
+        message.textContent = "Network Failure"
+    }
+}
+//sign-in function
+async function signIn(event) {
+    event.preventDefault()
+    let form = event.target
+}
+
+try {
+    let url = form.action.replace(location.origin, API_ORIGIN)
+    let res = await fetch(url, {
+        method: form.method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: loginNam.value,
+            password: loginPassword.value,
+        })
+
+    })
+    console.log('post signup response:', res.status, res.statusText)
+    message.textContent = res.statusText
+
+    let json = await res.json()
+
+    if (json.error) {
+        message.textContent = json.error
+    } else {
+        location.href = nextPageHref
+    }
+} catch (error) {
+    console.error('failed to post signup:', error)
+    message.textContent = "Network Failure"
+}
+
+
+
--- src/main/resources/static/readme.txt
+++
@@ -1,8 +0,0 @@
-index.html
-/nohorny/index.html
-/chat/index.html
-
-
-index.html
-nohorny.html
-chat.html
