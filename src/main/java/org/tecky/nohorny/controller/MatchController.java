c00020f53a01e6d4ea543773f3b1c5a608010c10
ECSCloud <tecky23project@gmail.com>
2022-11-16 20:55:48 +0800

    202211162055

---
+++ src/main/java/org/tecky/nohorny/controller/MatchController.java
@@ -0,0 +1,18 @@
+package org.tecky.nohorny.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MatchController {
+
+    @GetMapping("/match")
+    public String getMatch(Model model) {
+
+
+
+
+
+    }
+
+}
